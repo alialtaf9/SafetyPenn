9bc5bc964e027e54099087748f099bc38c2ed09a
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2014-03-30 15:21:15 -0400

    Connected to location services client

--- PennSafety/src/com/example/pennsafety/MainActivity.java
+++ PennSafety/src/com/example/pennsafety/MainActivity.java
@@ -4,6 +4,8 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.GoogleMap;
 
 import com.google.android.gms.maps.*;
@@ -14,9 +16,10 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
 import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -36,9 +39,26 @@ LocationListener {
 	 */
 	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
-	private LocationManager locationManager;
-	private LocationClient locationClient;
-	private Location currentLocation;
+	// Milliseconds per second
+	private static final int MILLISECONDS_PER_SECOND = 1000;
+	// Update frequency in seconds
+	public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+	// Update frequency in milliseconds
+	private static final long UPDATE_INTERVAL = MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+	// The fastest update frequency, in seconds
+	private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
+	// A fast frequency ceiling in milliseconds
+	private static final long FASTEST_INTERVAL = MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
+
+	// Define an object that holds accuracy and frequency parameters
+	private LocationRequest mLocationRequest;
+
+	private ConnectionResult connectionResult;
+	private LocationClient mLocationClient;
+	boolean mUpdatesRequested;
+	private SharedPreferences mPrefs;
+	private SharedPreferences.Editor mEditor;
+
 	private GoogleMap map;
 
 	@Override
@@ -46,103 +66,171 @@ LocationListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// set up map
-		map = ((MapFragment) getFragmentManager()
-				.findFragmentById(R.id.map)).getMap();
-		setUpMap();
-/*
-		// set up location client
-		locationClient = new LocationClient(this, this, this);
+		// Create the LocationRequest object
+		mLocationRequest = LocationRequest.create();
+		// Use high accuracy
+		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		// Set the update interval to 5 seconds
+		mLocationRequest.setInterval(UPDATE_INTERVAL);
+		// Set the fastest update interval to 1 second
+		mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
 
-		// get current location
-		currentLocation = locationClient.getLastLocation();
-		onStart(); */
-	}
-/*
-	// called when activity becomes visible
-	protected void onStart() {
-		super.onStart();
-		// connect client
-		locationClient.connect();
-	}
-
-	// called when activity disappears
-	protected void onStop() {
-		locationClient.disconnect();
-		super.onStop();
-	}
-*/
-	protected void setUpMap() {
-		// set map type to normal view mode
-		map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+		// Open the shared preferences
+		mPrefs = getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE);
+		// Get a SharedPreferences editor
+		mEditor = mPrefs.edit();
 
 		/*
-        map.setMyLocationEnabled(true);
+		 * Create a new location client, using the enclosing class to
+		 * handle callbacks.
+		 */
+		mLocationClient = new LocationClient(this, this, this);
+		// Start with updates turned off
+		mUpdatesRequested = false;
 
-        // get handler to system-level location services
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		// set up map
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-        // create a Criteria object to retrieve best provider
-        Criteria criteria = new Criteria();
-        String provider = locationManager.getBestProvider(criteria, true);
 
-        // obtain current location
-        Location currentLocation = locationManager.getLastKnownLocation(provider);
-        double latitude = currentLocation.getLatitude();
-        double longitude = currentLocation.getLongitude();
-        LatLng latLng = new LatLng(latitude, longitude);
+	}
 
-        // update map screen to current location
-        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        CameraUpdateFactory.zoomTo(20);*/
+	// called when activity becomes visible
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mLocationClient.connect();
 	}
 
 	@Override
-	public void onConnectionFailed(ConnectionResult arg0) {
-		// TODO Auto-generated method stub
-
+	protected void onPause() {
+		// Save the current setting for updates
+		mEditor.putBoolean("KEY_UPDATES_ON", mUpdatesRequested);
+		mEditor.commit();
+		super.onPause();
 	}
 
-	 /*
-     * Called by Location Services when the request to connect the
-     * client finishes successfully. At this point, you can
-     * request the current location or start periodic updates
-     */
-    @Override
-    public void onConnected(Bundle dataBundle) {
-        // Display the connection status
-        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+	/*
+	 * Called when the Activity is no longer visible at all.
+	 * Stop updates and disconnect.
+	 */
+	@Override
+	protected void onStop() {
+		// If the client is connected
+		if (mLocationClient.isConnected()) {
+			/*
+			 * Remove location updates for a listener.
+			 * The current Activity is the listener, so
+			 * the argument is "this".
+			 */
 
-    }
-    
+
+
+
+			//removeLocationUpdates(this);
+
+
+
+
+		}
+		/*
+		 * After disconnect() is called, the client is
+		 * considered "dead".
+		 */
+		mLocationClient.disconnect();
+		super.onStop();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		/*
+		 * Get any previous setting for location updates
+		 * Gets "false" if an error occurs
+		 */
+		if (mPrefs.contains("KEY_UPDATES_ON")) {
+			mUpdatesRequested = mPrefs.getBoolean("KEY_UPDATES_ON", false);
+			// Otherwise, turn off location updates
+		} else {
+			mEditor.putBoolean("KEY_UPDATES_ON", false);
+			mEditor.commit();
+		}
+	}
+
+	/*
+	 * Called by Location Services if the attempt to
+	 * Location Services fails.
+	 */	
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		/*
+		 * Google Play services can resolve some errors it detects.
+		 * If the error has a resolution, try sending an Intent to
+		 * start a Google Play services activity that can resolve
+		 * error.
+		 */
+		if (connectionResult.hasResolution()) {
+			try {
+				// Start an Activity that tries to resolve the error
+				connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+				/*
+				 * Thrown if Google Play services canceled the original
+				 * PendingIntent
+				 */
+			} catch (IntentSender.SendIntentException e) {
+				// Log the error
+				e.printStackTrace();
+			}
+		} else {
+			/*
+			 * If no resolution is available, display a dialog to the
+			 * user with the error.
+			 */
+
+
+
+			//showErrorDialog(connectionResult.getErrorCode());
+
+
+
+		}
+	}
+
+	/*
+	 * Called by Location Services when the request to connect the
+	 * client finishes successfully. At this point, you can
+	 * request the current location or start periodic updates
+	 */
+
+	@Override
+	public void onConnected(Bundle dataBundle) {
+		// Display the connection status
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+		// If already requested, start periodic updates
+		if (mUpdatesRequested) {
+			mLocationClient.requestLocationUpdates(mLocationRequest, this);
+		}
+	}
+
+	/*
+	 * Called by Location Services if the connection to the
+	 * location client drops because of an error.
+	 */
+
 	@Override
 	public void onDisconnected() {
-		// TODO Auto-generated method stub
-
+		Toast.makeText(this, "Disconnected. Please re-connect.",
+				Toast.LENGTH_SHORT).show();
 	}
 
+	// Define the callback method that receives location updates
 	@Override
 	public void onLocationChanged(Location location) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onProviderDisabled(String arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onProviderEnabled(String arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-		// TODO Auto-generated method stub
-
+		// Report to the UI that the location was updated
+		String msg = "Updated Location: " +
+				Double.toString(location.getLatitude()) + "," +
+				Double.toString(location.getLongitude());
+		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
 	}
 
 
@@ -166,25 +254,24 @@ LocationListener {
 		}
 	}
 
-	
+
 
 	/*
 	 * Handle results returned to the FragmentActivity
 	 * by Google Play services
 	 */
-/*
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		// Decide what to do based on the original request code
 		switch (requestCode) {
 
 		case CONNECTION_FAILURE_RESOLUTION_REQUEST :
-			
-		 // If the result code is Activity.RESULT_OK, try to connect again
-		  *
+
+			// If the result code is Activity.RESULT_OK, try to connect again
 			switch (resultCode) {
 			case Activity.RESULT_OK :
-				
+
 
 				//try the request again
 
@@ -207,7 +294,7 @@ LocationListener {
 			// Google Play services was not available for some reason
 		} else {
 			// Get the error code
-			int errorCode = ConnectionResult.getErrorCode();
+			int errorCode = connectionResult.getErrorCode();
 			// Get the error dialog from Google Play services
 			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
 					errorCode,
@@ -217,15 +304,18 @@ LocationListener {
 			// If Google Play services can provide an error dialog
 			if (errorDialog != null) {
 				// Create a new DialogFragment for the error dialog
-				ErrorDialogFragment errorFragment =
-						new ErrorDialogFragment();
+				ErrorDialogFragment errorFragment =	new ErrorDialogFragment();
 				// Set the dialog in the DialogFragment
 				errorFragment.setDialog(errorDialog);
 				// Show the error dialog in the DialogFragment
-				errorFragment.show(getSupportFragmentManager(),
-						"Location Updates");
+
+
+				//errorFragment.show(getSupportFragmentManager(), "Location Updates");
+
+
 			}
+			return false;
 		}
 	}
-*/
+
 }
