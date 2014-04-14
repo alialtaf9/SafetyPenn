48c13fcd78cb7d2590c11577fb7c1804a30bd7ab
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2014-04-14 01:13:28 -0400

    Map view initializes at Penn campus; view  updates view automatically based on location

--- PennSafety/src/com/example/pennsafety/MainActivity.java
+++ PennSafety/src/com/example/pennsafety/MainActivity.java
@@ -1,48 +1,26 @@
 package com.example.pennsafety;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.location.LocationClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 
 import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import android.app.Activity;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentSender;
-import android.content.SharedPreferences;
-import android.location.Address;
 import android.location.Criteria;
-import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.Menu;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends FragmentActivity {	
+public class MainActivity extends FragmentActivity implements LocationListener {	
 	private GoogleMap googleMap;
+	private LocationManager locationManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -58,51 +36,107 @@ public class MainActivity extends FragmentActivity {
 			googleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 			// Make another check for map initiation
 			if (googleMap != null) {
+				// initialize map at penn
+				double pennLat = 39.9539;
+				double pennLong = -75.1930;
+				LatLng pennLatLng = new LatLng(pennLat, pennLong);
+
+				// Show current location in map, zoom appropriately, and place marker
+				googleMap.moveCamera(CameraUpdateFactory.newLatLng(pennLatLng));
+				googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+				
+				
 				setUpMap();
 			}
 		}
 	}
 
 	private void setUpMap() {
+		System.out.println("Test to see how soon this happens");
+		
+		
 		// Enable MyLocation Layer
 		googleMap.setMyLocationEnabled(true);
 
 		// Get LocationManager
-		LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		
+	    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 400, 1000, this);     
 
+	    // see if map has gps enabled
+	    if (locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER) == null) {
+			Toast.makeText(this, "Not Connected to Current Location", Toast.LENGTH_SHORT).show();
+
+	    }
+	    
+		/*
 		// Create Criteria object to retrieve provider and find best provider
 		Criteria criteria = new Criteria();
 		String provider = locationManager.getBestProvider(criteria, true);
-
+/*
 		// Get current location
 		Location myLocation = locationManager.getLastKnownLocation(provider);
 
 		// Error handling for if app cannot find current location
 		if (myLocation == null) {
 			Toast.makeText(this, "Not Connected to Current Location", Toast.LENGTH_SHORT).show();
-		}
+		}*/
 		// find current location if myLocation is not null
-		else {
+		//else {
+		/*
 			// Get latitude and longitutde of location and create a LatLng
 			double latitude = myLocation.getLatitude();
 			double longitude = myLocation.getLongitude();
 			LatLng latLng = new LatLng(latitude, longitude);
-			
+
 			// Show current location in map, zoom appropriately, and place marker
 			googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 			googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
-			googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("EDIT THIS WITH ADDRESS"));
-		}
-
-		/*
-		double latitude = 39.9539;
-		double longitude = -75.1930;*/
-
+			googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Current Location"));
+		 */
 	}
+
+	//}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onLocationChanged(Location myLocation) {
+		System.out.println("Location changed happened");
+
+		if (myLocation == null) {
+			Toast.makeText(this, "Not Connected to Current Location", Toast.LENGTH_SHORT).show();
+		} else {
+
+			// Get latitude and longitutde of location and create a LatLng
+			double latitude = myLocation.getLatitude();
+			double longitude = myLocation.getLongitude();
+			LatLng latLng = new LatLng(latitude, longitude);
+
+			// Show current location in map, zoom appropriately, and place marker
+			googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+			googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+		//	googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Current Location"));
+
+		}
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		Toast.makeText(this, "Not Connected to Current Location", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+
 	}
 }
