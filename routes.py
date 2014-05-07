2f15239e9a11b59c999cfc33495249c72c1d3434
Ali Altaf <alialtaf@seas.upenn.edu>
2014-05-07 17:17:05 -0400

    final commit

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, url_for
+from flask import Flask, render_template, request, redirect, session, url_for, jsonify
 from twilio.rest import TwilioRestClient
 import boto
 import json
@@ -6,16 +6,12 @@ import os
 
 app = Flask(__name__)
 
-#eventually change this to use os for the sake of security
-# add requirements to a textfile
 sdb = boto.connect_sdb(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'])
 client = TwilioRestClient(os.environ['TWILIO_KEY'], os.environ['TWILIO_SECRET'])
 members = sdb.get_domain('members')
 notifications = sdb.get_domain('notifications')
 members_list = {}
 notifications_list = []
-
-
 
 for item in members:
   members_list[item.name] = {'name': item['name'], 'gender': item['gender'], 'height' : item['height'], 'weight' : item['weight'], 'eye_color' : item['eye_color'], 'hair_color' : item['hair_color'], 'picture' : item['picture'], 'user_number' : item['user_number'], 'emergency_number' : item['emergency_number']}
@@ -47,47 +43,66 @@ def add_member():
   new_member['weight'] = request.form['weight']
   new_member['eye_color'] = request.form['eye_color']
   new_member['hair_color'] = request.form['hair_color']
-  new_member['picture'] = request.form['picture']
+  new_member['picture'] = "http://" + request.form['picture']
   new_member['user_number'] = request.form['user_number']
   new_member['emergency_number'] = request.form['emergency_number']
   new_member.save()
-  members_list[request.form['email']] = {'name': request.form['name'], 'gender': request.form['gender'], 'height' : request.form['height'], 'weight' : request.form['weight'], 'eye_color' : request.form['eye_color'], 'hair_color' : request.form['hair_color'], 'picture' : request.form['picture'], 'user_number': request.form['user_number'], 'emergency_number': request.form['emergency_number']}
-  return redirect(url_for('home'))
+  members_list[request.form['email']] = {'name': request.form['name'], 'gender': request.form['gender'], 'height' : request.form['height'], 'weight' : request.form['weight'], 'eye_color' : request.form['eye_color'], 'hair_color' : request.form['hair_color'], 'picture' : "http://" + request.form['picture'], 'user_number': request.form['user_number'], 'emergency_number': request.form['emergency_number']}
+  return jsonify([])
 
 #when a timer goes off a notification is added to the database
 @app.route('/addnotification', methods=['POST'])
 def add_notification():
-  print "********** 1"
+  print request.form
   new_notification = notifications.new_item(request.form['email'])
-  print "********** 2"
   new_notification['latitude'] = request.form['latitude']
-  print "********** 3"
   new_notification['longitude'] = request.form['longitude']
-  print "********** 4"
   new_notification['type'] = request.form['type']
-  print "********** 5"
   if request.form['type'] == 'timer':
-    print "********** 6"
     new_notification['message'] = "A timer has gone off!"
-    print "********** 7"
     lat = new_notification['latitude']
     longitude = new_notification['longitude']
     location = "http://maps.google.com/?ie=UTF8&q=e@" + lat + "," + longitude
-    print "********** 8"
-    bod = "SafetyPenn Alert! "# + members_list[request.form['email']]['name'] + " is in trouble! You can find " + members_list[request.form['email']]['name'] + " here: " + location
-    print bod
-    #client.messages.create(to=members_list[notification[4]]['user_number'], from_="+12674158806", body="An escort has been sent to your location")
-    print "********** 9"
+    bod = "SafetyPenn Alert!" + members_list[request.form['email']]['name'] + " is in trouble! You can find " + members_list[request.form['email']]['name'] + " here: " + location
+    client.messages.create(to=members_list[request.form['email']]['emergency_number'], from_="+12674158806", body=bod)
   else:
     new_notification['message'] = "An escort has been requested"
-  print "********** 10"
   new_notification.save()
-  print "********** 11"
-  notification = [new_notification['latitude'], new_notification['longitude'], members_list[request.form['email']], new_notification['message'], request.form['type']]
-  print "********** 12"
-  notifications_list.append(notification)
-  print "********** 13"
-  return redirect(url_for('home'))
+
+  notification = [new_notification['latitude'], new_notification['longitude'], members_list[request.form['email']], new_notification['message'], request.form['email'], request.form['type']]
+  inList = False
+  for notif in notifications_list:
+    if notif[4] == request.form['email']:
+      notif = notification
+      inList = True
+  if inList is False:
+    notifications_list.append(notification)
+  return jsonify([])
+
+@app.route('/edit', methods=['POST'])
+def edit():
+  print 'here'
+  print request.form
+  email = request.form['email']
+  user = members.get_item(email)
+  print email
+  print user
+  try:
+    picture = "http://" + request.form['picture']
+    user['picture'] = picture
+    members_list[email]['picture'] = picture
+    user.save()
+  except KeyError:
+    print "do nothing"
+  try:
+    emergency = request.form['emergency_number']
+    user['emergency_number'] = emergency
+    members_list[email]['emergency_number'] = emergency
+    user.save()
+  except KeyError:
+    print "do nothing"
+  return jsonify([])
+
 
 @app.route('/remove')
 def remove_notification():
@@ -99,7 +114,7 @@ def remove_notification():
       print "found notification"
       if notification[5] == 'escort':
         print "print notification is escort"
-        client.messages.create(to="+12672374105", from_="+12674158806", body="An escort has been sent to your location")
+        client.messages.create(to=members_list[notification[4]]['user_number'], from_="+12674158806", body="An escort has been sent to your location")
         print "message sent"
       notifications_list.remove(notification)
   notifications.delete_item(notifications.get_item(id))
